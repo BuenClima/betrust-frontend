b86e23cb31e0bec482c483816170109bf65dbaf3
Diego Martin Hernandez <diegomartin9495@gmail.com>
2024-01-02 12:38:06 +0000

    AppBar tested

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,7 +1,7 @@
 /// <reference types="@testing-library/jest-dom" />
 
+import mediaQuery from 'css-mediaquery'
 import nodeFetch, { Request, Response } from 'node-fetch'
-
 Object.assign(global, {
   fetch: nodeFetch,
   Request,
@@ -16,3 +16,22 @@ import { afterEach } from 'vitest'
 afterEach(() => {
   cleanup()
 })
+
+function createMatchMedia(width: number, height: number) {
+  return (query: string) => {
+    return {
+      matches: mediaQuery.match(query, { width, height }),
+      media: '',
+      addListener: () => {},
+      removeListener: () => {},
+      onchange: () => {},
+      addEventListener: () => {},
+      removeEventListener: () => {},
+      dispatchEvent: () => true
+    }
+  }
+}
+
+export function resizeScreenSize(width: number, height: number) {
+  window.matchMedia = createMatchMedia(width, height)
+}
